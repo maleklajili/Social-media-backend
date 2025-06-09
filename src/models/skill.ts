793e0dbd5d3eddb06ecd019901b8e9bcfcf269b6
ag-beyrem <beyrem.agrebi@proservices-training-company.com>
2025-06-09 11:55:33 +0100

    fix level value

--- src/models/skill.ts
+++ src/models/skill.ts
@@ -2,10 +2,11 @@ import type { ObjectId } from "mongodb";
 import type { BaseModel } from "./base/base-model";
 
 export enum SkillLevel {
-  Débutant = "Débutant",
-  Intermédiaire = "Intermédiaire",
-  Avancé = "Avancé",
-  Expert = "Expert",
+  Débutant = "debutant",
+  Intermédiaire = "intermediaire",
+  Avancé = "avance",
+  Expert = "expert",
+  Natif = "natif",
 }
 
 export interface Skill extends BaseModel {
