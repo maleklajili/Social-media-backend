d98772fbe4e27f96a06758d9fc2f0a50af3bc145
Ghnimi Hassen <hassen.ghnimi@proservices-training-company.com>
2026-01-05 10:55:01 +0100

    add model company

---
+++ src/models/company.ts
@@ -0,0 +1,43 @@
+import type { ObjectId } from "mongodb";
+import type { BaseModel } from "./base/base-model";
+
+export interface Company extends BaseModel {
+  userId: ObjectId;
+  name: string;
+  industry: string;
+  description: string;
+  shortDescription: string;
+  website: string;
+  foundedYear?: string;
+  size: string;
+  location: string;
+  address: string;
+  phone: string;
+  email: string;
+  logo: string;
+  coverImage: string;
+  socialMedia: {
+    linkedin: string;
+    twitter: string;
+    facebook: string;
+    instagram: string;
+  };
+  keywords: string[];
+  status: "active" | "draft" | "archived";
+  verified: boolean;
+  verificationStatus: "verified" | "pending" | "rejected" | "not_requested";
+  verificationDocuments?: {
+    file: string;
+    type: string;
+    name: string;
+  }[];
+  verificationNotes?: string;
+  stats: {
+    views: number;
+    followers: number;
+    jobApplications: number;
+    messages: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
