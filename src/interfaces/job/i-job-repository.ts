ce2878c1a64acd4a27712d52c540618cf1bdd238
Ghnimi Hassen <hassen.ghnimi@proservices-training-company.com>
2026-01-07 11:47:43 +0100

    add job repository

---
+++ src/interfaces/job/i-job-repository.ts
@@ -0,0 +1,17 @@
+import type { ObjectId } from "mongodb";
+import type { Job } from "../../models/job";
+
+export interface IJobRepository {
+  addJob(job: Job): Promise<void>;
+  updateJob(job: Job): Promise<void>;
+  deleteJob(id: ObjectId, userId: ObjectId): Promise<boolean>;
+  getJobsByCompanyId(companyId: ObjectId): Promise<Job[]>;
+  getJobsByUserId(userId: ObjectId): Promise<Job[]>;
+  getJobById(id: ObjectId): Promise<Job | null>;
+  getActiveJobs(filters?: {
+    location?: string;
+    contractType?: string;
+    experience?: string;
+    skills?: string[];
+  }): Promise<Job[]>;
+}
---
+++ src/interfaces/job/i-job-service.ts
@@ -0,0 +1,13 @@
+import type { ObjectId } from "mongodb";
+import type { Job } from "../../models/job";
+
+export interface IJobService {
+  addJob(userId: ObjectId, job: Job): Promise<Response>;
+  updateJob(userId: ObjectId, jobId: ObjectId, job: Job): Promise<Response>;
+  deleteJob(userId: ObjectId, jobId: ObjectId): Promise<Response>;
+  getJobsByCompanyId(companyId: ObjectId): Promise<Response>;
+  getJobsByUserId(userId: ObjectId): Promise<Response>;
+  getJobById(jobId: ObjectId): Promise<Response>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getActiveJobs(filters?: Record<string, any>): Promise<Response>;
+}
